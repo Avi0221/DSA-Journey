dc5deb455be1468016e5d2a4e707a4699e372fe2
Avinash <avinash2596360@gmail.com>
2025-10-07 23:52:01 +0530

    Added Backtracking Folder

---
+++ Backtracking/find_subsets.java
@@ -0,0 +1,27 @@
+public class find_subsets {
+    public static void findsubset(String str,String ans,int i){
+        // BAse case
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("Null");
+            }
+            else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //Recursion
+
+        // Chooice--Yes
+        findsubset(str, ans+str.charAt(i), i+1);
+
+        // Choice -- No
+        findsubset(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findsubset(str, "", 0);
+    }
+}
