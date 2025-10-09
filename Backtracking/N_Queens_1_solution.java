80900f0575531c0b540c2123b7bed80de23863fc
Avinash <avinash2596360@gmail.com>
2025-10-10 01:01:14 +0530

    New files added

--- Backtracking/N_Queens_1_solution.java
+++ Backtracking/N_Queens_1_solution.java
@@ -1,5 +1,4 @@
-public class N_Queens {
-
+public class N_Queens_1_solution {
     public static boolean isSafe(char board[][],int row,int col){
         // Vertical UP
         for(int i=row-1;i>=0;i--){
@@ -23,21 +22,24 @@ public class N_Queens {
         return true;
     }
 
-    public static void nQueens(char board[][],int row){
+    public static boolean  nQueens(char board[][],int row){
         // base
         if(row==board.length){
             // printBoard(board);
             count++;
-            return;
+            return true;
         }
         // Column loop
         for(int j=0;j<board.length;j++){
             if(isSafe(board,row,j)){
                 board[row][j] = 'Q';
-                nQueens(board, row+1); //Funcation Call
+                if(nQueens(board, row+1)){
+                    return true;
+                } //Funcation Call
                 board[row][j]='X'; //Backtracking Step
             }
         }
+        return false;
     }
 
     public static void printBoard(char board[][]){
@@ -54,7 +56,7 @@ public class N_Queens {
 
 
     public static void main(String[] args) {
-        int n = 4;
+        int n = 2;
         char board[][] = new char[n][n];
         // initialize
         for(int i=0;i<n;i++){
@@ -63,7 +65,14 @@ public class N_Queens {
             }
         }
 
-        nQueens(board,0);
-        System.out.println("Total Number of ways to solve N Queens :"+ count);
+        if(nQueens(board,0)){
+            System.out.println("Solution is Possible!!");
+            printBoard(board);
+        }
+        else{
+            System.out.println("Solution is not Possible!!!!");
+        }
+        // System.out.println("Total Number of ways to solve N Queens :"+ count);
     }
+
 }
